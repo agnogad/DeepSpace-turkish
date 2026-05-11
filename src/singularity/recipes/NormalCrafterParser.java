8d0d2b68bd265028230ae37705546afed01d4698
agnogad <seasmoffi@gmail.com>
2026-05-11 23:38:24 +0400

    fix error

--- src/singularity/recipes/NormalCrafterParser.java
+++ src/singularity/recipes/NormalCrafterParser.java
@@ -107,9 +107,9 @@ public class NormalCrafterParser extends RecipeParser<NormalCrafter> {
     for (ObjectMap.Entry<BaseConsumers, BaseProducers> product : normalCrafter.getOptionalProducts()) {
       Recipe recipe = new Recipe(RecipeType.factory, getWrap(normalCrafter), product.key.getCraftTime());
       recipe.setSubInfo(t -> {
-        t.add("[accent]Ek Ikincil Uretim Ogeleri");
+        t.add(Core.bundle.get("misc.additionalSecondaryProduction"));
         if (!product.key.getOptionalAlwaysValid())
-          t.row().add("[lightgray]Ana uretim calisir durumda olmali").padTop(4);
+          t.row().add(Core.bundle.get("misc.mainProductionMustBeWorking")).padTop(4);
       });
 
       for (BaseProduce<?> produce : product.value.all()) {
